6c67301905e3fe95765b96e638156deba96f2c86
Logan Sobczak <loganhsobczak@gmail.com>
2015-11-27 14:56:05 -0500

    added libraries, css, and the block plugin implmentation

---
+++ src/Plugin/Block/ModuleExampleBlock.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Provides a 'Module Example' Block
+ *
+ * @Block(
+ *   id = "module_example",
+ *   admin_label = @Translation("Module Example Block"),
+ * )
+ */
+
+namespace Drupal\module_example\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+class ModuleExampleBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $output = [
+      '#type' => 'markup',
+      '#theme' => 'module__example',
+      '#thing' => 'taco',
+      '#attached' => [
+        'library' => ['module_example/module_example_main']
+      ]
+    ];
+    return $output;
+  }
+}
+?>
